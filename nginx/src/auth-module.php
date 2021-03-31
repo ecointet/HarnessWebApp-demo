22794d48aecfc3d09312bbfe9077b4a3bd267c6a
ecointet2 <81680534+ecointet2@users.noreply.github.com>
2021-03-31 19:46:04 +0200

    New version as a suggestion

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1;
-$auth_message="Welcome in our World!";
+$auth_version=2;
+$auth_message="Welcome in my World!";
 
 //New Auth Module is version 2
 //Old Auth Module is version 1
