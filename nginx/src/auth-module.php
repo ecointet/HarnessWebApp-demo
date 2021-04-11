2b6783f297fa6d1c035913afabe629e9fbbcd73a
ecointet2 <81680534+ecointet2@users.noreply.github.com>
2021-04-11 22:57:56 +0200

    Picture in HD (better quality)

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
+$auth_version=2; // app Version (1 or 2)
 $auth_icon=""; //http://image.png
-$auth_message=""; //Insert a message
+$auth_message="Welcome France"; //Insert a message
 
 ?>
