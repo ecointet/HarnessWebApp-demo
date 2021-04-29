dd37efc97299fecad2f2fbdd6abc77851bba1848
ecointet2 <81680534+ecointet2@users.noreply.github.com>
2021-04-29 09:52:08 +0200

    New Version with a better image

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
 $auth_version=1; // app Version (1 or 2)
-$auth_icon=""; //http://image.png
-$auth_message=""; //Insert a message
+$auth_icon="https://image-uviadeo.journaldunet.com/image/450/1581986568/1211529.jpg"; //http://image.png
+$auth_message="Be Proud!"; //Insert a message
 
 ?>
