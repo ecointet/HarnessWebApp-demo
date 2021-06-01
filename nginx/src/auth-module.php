3ab7fb97f95a0e294c1035594f20638bdc52ddaa
ecointet2 <81680534+ecointet2@users.noreply.github.com>
2021-06-01 09:57:16 +0200

    New logo

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; //http://image.png
-$auth_message=""; //Insert a message
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.teamjumbovisma.com/uploads/OG/_800x800_crop_center-center_82_none/og_image.png?mtime=1545234556"; //http://image.png
+$auth_message="Visma Team!"; //Insert a message
 
 ?>
