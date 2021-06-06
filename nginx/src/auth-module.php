4dc83ce654df905bcd94ca835b3a6f56c12d19af
ecointet2 <81680534+ecointet2@users.noreply.github.com>
2021-06-06 13:20:20 +0200

    Verification Module - integration

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,6 +1,6 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://cdn.theorg.com/16f57b77-1e3b-4755-a8e7-25e5ec937b61_small.jpg";
+$auth_message="This is a better app!"; // Landing page
 ?>
