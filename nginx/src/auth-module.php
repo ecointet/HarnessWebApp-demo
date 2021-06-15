108295f8a7906c4579a0a18415430c38931cc33e
ecointet2 <81680534+ecointet2@users.noreply.github.com>
2021-06-15 11:53:11 +0200

    New Landing Page

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
-$my_link=""; // your link
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://i.ytimg.com/vi/SpHtT15BUoY/maxresdefault.jpg";
+$auth_message="Vive la FRANCE"; // Landing page
+$my_link="test32"; // your link
 ?>
