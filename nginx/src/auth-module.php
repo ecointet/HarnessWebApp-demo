02648e17e63a8e0217be7c53ede4c06616cf1788
Peter Hack <peterhack+github@gmail.com>
2021-06-17 15:46:47 -0400

    pete to test

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.clipartkey.com/mpngs/m/69-693588_drunk-emoji-png-icon-drunk-emoji-png.png";
+$auth_message="You have arrived!"; // Landing page
 $my_link=""; // your link
 ?>
