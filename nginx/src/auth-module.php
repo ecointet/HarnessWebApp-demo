8f9ff1b5f535fedfd19b1a7659ae12fef5b3db8c
Peter Hack <peterhack+github@gmail.com>
2021-06-22 09:49:56 -0400

    updated version

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
-$auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://bit.ly/35Jxmf2";
+$auth_message="Viva La France!"; // Landing page
 $my_link=""; // your link
 ?>
